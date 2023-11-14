794eca38bf4d18d8bf6685e92a89944791d78e65
rrivas <rrivas@utec.edu.pe>
2023-11-14 08:58:07 -0500

    update push/pop

--- heap.h
+++ heap.h
@@ -12,11 +12,13 @@ namespace utec {
 
     template <
             typename T,
-            template <typename ...> class Container=vector
+            template <typename ...> class Container=vector,
+            typename Compare=std::less<T>
     >
     class heap {
         // Contanedor
         Container<T> cnt;
+        Compare cmp;
         // Metodos auxiliares
         static size_t get_left_index(size_t parent_index) { return parent_index * 2; }
         static size_t get_right_index(size_t parent_index) { return parent_index * 2 + 1; }
@@ -33,7 +35,7 @@ namespace utec {
             auto left_index = get_left_index(parent_index);
             if (has_right_child(parent_index)) {
                 auto right_index = get_right_index(parent_index);
-                return cnt[right_index] < cnt[left_index]? left_index: right_index;
+                return cmp(cnt[right_index],cnt[left_index])? left_index: right_index;
             }
             return left_index;
         }
@@ -44,7 +46,7 @@ namespace utec {
             if (is_root(index)) return;
             // Obtener el indice del padre
             auto parent_index = get_parent_index(index);
-            if (cnt[index] < cnt[parent_index]) return;
+            if (cmp(cnt[index],cnt[parent_index])) return;
 
             // CONDICIONES RECURSIVAS
             swap(cnt[index], cnt[parent_index]);
@@ -58,14 +60,31 @@ namespace utec {
             if (!has_children(index)) return;
             // Selecciona al hijo si es MAX: Mayor y si esl MIN: Menor
             auto child_selected_index = get_selected_index(index);
-            if (cnt[child_selected_index] < cnt[index]) return;
+            if (cmp(cnt[child_selected_index], cnt[index])) return;
 
             // CONDICIONES RECURSIVAS
             swap(cnt[child_selected_index], cnt[index]);
             percolate_down(child_selected_index);
         }
+
+        void heapify(size_t index) {
+            // Condicion inicial (obtener el indice del padre
+            auto cur_index = get_parent_index(index);
+            while (cur_index >= 1) {
+                percolate_down(cur_index);
+                --cur_index;
+            }
+        }
     public:
         heap(): cnt(1) {}
+        heap(Container<T> c): cnt(c.size() + 1) {
+            copy(begin(c), end(c), begin(cnt) + 1); // O(n)
+            heapify(size()); // O(n)
+        }
+        heap(Container<T> c, Compare cmp): cnt(c.size() + 1), cmp(cmp) {
+            copy(begin(c), end(c), begin(cnt) + 1); // O(n)
+            heapify(size()); // O(n)
+        }
         size_t size() { return cnt.size() - 1; }
         bool empty() { return size() == 0; }
         T& top() { return cnt[1];}
@@ -80,7 +99,15 @@ namespace utec {
             percolate_down(1);
         }
 
-
+        bool replace(T old, T new_value) {
+            auto it = find(begin(cnt)+1, end(cnt), old);
+            if (it != end(cnt)) {
+                *it = new_value;
+                heapify(size());
+                return true;
+            }
+            return false;
+        }
     };
 
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <vector>
+#include "heap.h"
+using namespace std;
+
+void ejemplo_1 () {
+    utec::heap<int> h;
+    vector v {10, 7, 27, 50, 3, -1, 2, 10};
+    for (const auto& item: v)
+        h.push(item);
+
+    while(!h.empty()) {
+        cout << h.top() << " ";
+        h.pop();
+    }
+}
+
+void ejemplo_2 () {
+    vector v {10, 7, 27, 50, 3, -1, 2, 10};
+    utec::heap<int> h(v);
+
+    while(!h.empty()) {
+        cout << h.top() << " ";
+        h.pop();
+    }
+    cout << endl;
+    utec::heap<int> h1(v);
+    h1.replace(7, 20);
+    while(!h1.empty()) {
+        cout << h1.top() << " ";
+        h1.pop();
+    }
+}
+
+void ejemplo_3 () {
+    vector v {10, 7, 27, 50, 3, -1, 2, 10};
+    utec::heap<int, vector, greater<>> h(v);
+
+    while(!h.empty()) {
+        cout << h.top() << " ";
+        h.pop();
+    }
+}
+
+void ejemplo_4 () {
+    vector v {10, 7, 27, 50, 3, -1, 2, 10};
+    auto compare_greater = [](auto a, auto b) { return a > b; };
+    utec::heap<int, vector, decltype(compare_greater)> h(v, compare_greater);
+
+    while(!h.empty()) {
+        cout << h.top() << " ";
+        h.pop();
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ejemplo_1();
+    cout << endl;
+    ejemplo_2();
+    cout << endl;
+    ejemplo_3();
+    cout << endl;
+    ejemplo_4();
     return 0;
 }
